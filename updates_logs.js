840693692dba0ccd618861f5bcbd132a6ac25e97
Kahr-Noss <maxime.collodel@orange.fr>
2017-08-21 17:32:46 +0900

    Add files via upload

---
+++ updates_logs.js
@@ -0,0 +1,18 @@
+const updates = {
+  update_1:{
+    implementationDate:"2017-08-17T15:15:32+09:00",
+    questsAdded:["A3"],
+    questsReqModified:{
+      "A2":{
+        removed:["B1"],
+        added:["A1"]
+      }
+    }
+  },
+
+  update_2:{
+    implementationDate:"2017-08-07T15:15:32+09:00",
+    questsAdded:["B2"],
+    questsReqModified:{}
+  }
+}
